e1dc048ac747c2ea150ed897e68edf762f508de5
fukatataku <fukata.taku@gmail.com>
2013-01-26 20:24:57 +0900

    fix

--- app.rb
+++ app.rb
@@ -74,7 +74,7 @@ get "/modify_test" do
     target_strings = doc.elements["/en-note/div"].get_text
     
     # MagicServerを叩く
-    req_str = URI.encode("http://cbkx481-abj-app000.c4sa.net/api/magic.json?guid=#{guid}&string=#{target_strings}")
+    req_str = "http://cbkx481-abj-app000.c4sa.net/api/magic.json?guid=#{guid}&string=#{target_strings}"
     url = URI.parse(URI.encode(req_str))
     req = Net::HTTP::Get.new(url.path+"?"+url.query)
     res = Net::HTTP.start(url.host, url.port) {|http|
@@ -86,6 +86,7 @@ get "/modify_test" do
     
     # HTMLをENMLに変換する
     plan_enml = html2enml(plan_html)
+    puts plan_enml
     
     # 既存のノートに上書き
     targetNote.content = plan_enml
