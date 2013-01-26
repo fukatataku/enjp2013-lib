16ba712968cb75eaba7a0a92df260173a9ff7021
fukatataku <fukata.taku@gmail.com>
2013-01-26 10:00:14 +0900

    add test strings

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
-# -*- encoding: utf-8 -*-
+﻿# -*- encoding: utf-8 -*-
 require "rubygems"
 require "sinatra"
 
 get "/" do
-    "Hello NIFTYCloud C4SA"
+    "Hello NIFTYCloud C4SA @ Evernote Hackathon 2013"
 end
 
 get "/index" do
